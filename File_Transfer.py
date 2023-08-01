5bb1301b52f530b7750e3b99c11f4e32f365fd82
VigneshC <127950528+VigneshC8597@users.noreply.github.com>
2023-08-01 10:55:50 +0530

    Update File_Transfer.py

--- File_Transfer.py
+++ File_Transfer.py
@@ -1,15 +1,1 @@
-import shutil
-
-def send_file(source_path, destination_path):
-    try:
-        shutil.move(source_path, destination_path)
-        print("File sent successfully!")
-    except FileNotFoundError:
-        print("File not found.")
-    except Exception as e:
-        print(f"Error occurred: {e}")
-
-if __name__ == "__main__":
-    source_path = "D:\\Godrej_application_count\\Batch Count\\Matching Count\\AMCPCP_828.xlsx"
-    destination_path = "D:\\Godrej_application_count\\Batch Count\\MisMatching Count"
-    send_file(source_path, destination_path)
+hello world
